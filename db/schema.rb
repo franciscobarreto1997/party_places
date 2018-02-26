85347923c5d567f04007bb58e333d19a34e39aab
jose monteiro <joemonteiro@verizon.net>
2018-02-26 17:05:48 +0000

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180226141241) do
-=======
 ActiveRecord::Schema.define(version: 20180226141253) do
->>>>>>> 7e4d542f2434439ce8b2914e7669cbe98d14a473
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,19 +18,11 @@ ActiveRecord::Schema.define(version: 20180226141253) do
   create_table "bookings", force: :cascade do |t|
     t.string "date"
     t.bigint "user_id"
-<<<<<<< HEAD
-    t.string "venue"
-    t.string "references"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-=======
     t.bigint "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
->>>>>>> 7e4d542f2434439ce8b2914e7669cbe98d14a473
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -48,12 +36,6 @@ ActiveRecord::Schema.define(version: 20180226141253) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-<<<<<<< HEAD
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-=======
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -69,7 +51,6 @@ ActiveRecord::Schema.define(version: 20180226141253) do
     t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> 7e4d542f2434439ce8b2914e7669cbe98d14a473
   end
 
   create_table "venues", force: :cascade do |t|
@@ -85,9 +66,6 @@ ActiveRecord::Schema.define(version: 20180226141253) do
   end
 
   add_foreign_key "bookings", "users"
-<<<<<<< HEAD
-=======
   add_foreign_key "bookings", "venues"
->>>>>>> 7e4d542f2434439ce8b2914e7669cbe98d14a473
   add_foreign_key "reviews", "bookings"
 end
