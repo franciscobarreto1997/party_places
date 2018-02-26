9ac3ae000ca0dac5e3b241864b48e2511977d3c6
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-02-26 15:26:53 +0000

    created the controllers with the respective actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/show'
+
+  get 'bookings/update'
+
+  get 'bookings/edit'
+
+  get 'bookings/delete'
+
+  get 'venues/index'
+
+  get 'venues/show'
+
+  get 'venues/search'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
